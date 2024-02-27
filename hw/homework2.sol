a5a4adf72943f344838b5e7951e740ec400ac273
Hank <hank.butler@protonmail.com>
2024-02-27 15:24:37 -0700

    more dynamic way of removing elements

--- hw/homework2.sol
+++ hw/homework2.sol
@@ -4,20 +4,31 @@ pragma solidity ^0.8.7;
 
 contract ArrayFilter {
     
-    uint256[] public oldArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
     uint256[] public arr;
 
-    function removeElements() public {
-        uint i;
-        for (i; i < oldArr.length; i++) {
-            if (i != 2 && i != 7 && i != 8){
-                arr.push(i);
-            }
+    function removeElement(uint _index) public {
+        require(_index < arr.length);
+
+        for (uint i = _index; i < arr.length - 1; i++){
+            arr[i] = arr[1+1];
         }
+        arr.pop();
     }
 
     function getArray() public view returns (uint[] memory){
         return arr;
+    }
+
+    function test() external {
+        arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+        removeElement(2);
+        removeElement(7);
+        removeElement(8);
+        assert(arr.length == 9);
+
+        arr = [1];
+        removeElement(0);
+        assert(arr.length == 0);
     }
 
 }
