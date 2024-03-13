e4b31965bb78bb9eb212e9d4582934017d91e5a1
Hank <hank.butler@protonmail.com>
2024-03-12 20:48:56 -0600

    added onlyOwner modifier, working on constructor

--- sol_files/lesson2.sol
+++ sol_files/lesson2.sol
@@ -2,15 +2,31 @@
 
 pragma solidity ^0.8.0;
 
+
+
 contract Score {
 
+    address owner;
+
     uint score = 5;
+
+    modifier onlyOwner {
+    if (msg.sender == owner) {
+        _; // placeholder for function to be called if modifier invoked
+        }
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    
 
     function getScore() public view returns (uint) {
         return score;
     }
 
-    function setScore(uint _new_score) public {
+    function setScore(uint _new_score) public onlyOwner {
         score = _new_score;
     }
 
