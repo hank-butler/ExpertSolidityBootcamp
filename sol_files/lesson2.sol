29265bee634f32040066fd86c2c50d3fcc45bb9d
Hank <hank.butler@protonmail.com>
2024-03-12 20:54:08 -0600

    deployed on sepolia via Remix

--- sol_files/lesson2.sol
+++ sol_files/lesson2.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+// Sepolia Etherscan: https://sepolia.etherscan.io/tx/0xe74b9cd753cb7577859cd1053fa9f95ae7719950e29924f472f682337404b1ad
 
 pragma solidity ^0.8.0;
 
